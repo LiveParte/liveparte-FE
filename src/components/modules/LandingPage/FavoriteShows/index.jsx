3d177d7b8665adccc42902b10397e5f546e34e8e
MAC <user@MACs-MacBook-Pro.local>
2024-02-21 19:35:00 +0100

    update

--- src/components/modules/LandingPage/FavoriteShows/index.jsx
+++ src/components/modules/LandingPage/FavoriteShows/index.jsx
@@ -6,20 +6,20 @@ export default function FavoriteShow() {
   return (
     <div className="pt-[56px] bg-[#060809]  pb-[80px] lg:pb-[112px]">
       <div className="px-[30px] lg:px-[96px] flex justify-between items-center mb-[40px] md:mb-[80px] lg:mb-[119px]">
-        <div style={{visibility:'hidden'}} className="hidden lg:block">
+        <div  className="hidden lg:block">
           <BannerIcon />
         </div>
         <div className="font-1 text-[28px] md:text-[35px] lg:text-[45px] font-bold uppercase text-white text-center md:leading-[56px]">
           Watch your favourite celebrity perform
           <br  className="hidden lg:block"/> live from any device
         </div>
-        <div className="w-[195px] hidden lg:block">
+        <div style={{visibility:'hidden'}} className="w-[195px] hidden lg:block">
           <BannerIcon />
         </div>
       </div>
 
-      <div className="flex justify-center gap-[51px] items-center px-[30px] lg:px-[104px] ">
-        <div className="w-[166px] hidden lg:block">
+      <div className="flex justify-center gap-[51px] items-start px-[30px] lg:px-[104px] ">
+        <div className="w-[166px] hidden lg:block" style={{visibility:'hidden'}}>
           <BannerIcon2 />
         </div>
         <div className="flex justify-center items-center text-center ">
@@ -30,7 +30,7 @@ export default function FavoriteShow() {
             classNameMain={`flex justify-center`}
           />
         </div>
-        <div className="hidden lg:block" style={{visibility:'hidden'}}>
+        <div className="hidden lg:block mt-5" >
           <BannerIcon2 />
         </div>
       </div>
