be675a03c6cc4e10a20e2ac60b9eae06f57655ac
JosiahKolawole <josiah.kolawole@fbistech.com>
2024-05-31 13:22:52 +0100

    switch to staging

--- src/store/baseApi/baseUrl.js
+++ src/store/baseApi/baseUrl.js
@@ -5,6 +5,7 @@ export const baseUrlProd =`https://backend.liveparte.com/v1/api/`;
  const payStackProd='pk_live_f9e91d7c1463cb421aaab647c6b1a5a8a8fe568e';
  export const payStack=isTestEnvironment?payStackDev:payStackProd;
  export const BASE_URL =isTestEnvironment? baseUrl:baseUrlProd ;
+ export const eventCopyLink=''
 //  publicKey: "pk_live_f9e91d7c1463cb421aaab647c6b1a5a8a8fe568e",
 //  // publicKey: "pk_test_9b34d7cad3b54108b6eb034c951d89366eadcc3d",
 //backend.liveparte.com
