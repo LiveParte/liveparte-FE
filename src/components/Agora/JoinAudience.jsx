bc16c18ea5df2386ef07e1df62691d26d235a093
JosiahKolawole <josiah.kolawole@fbistech.com>
2024-08-25 12:27:44 +0100

    add token

--- src/components/Agora/JoinAudience.jsx
+++ src/components/Agora/JoinAudience.jsx
@@ -14,6 +14,27 @@ import { useDispatch, useSelector } from "react-redux";
 import { selectLastEventAttended, setLastEventAttended } from "@/store/User";
 // import { selectLiveStreamEvent, setLastEventAttended } from '@/store/Event';
 
+const showTokens = [
+  {
+    tokenId:
+      "007eJxTYMh1nTHtap38Czunqa3F/pO+2jLu3JOor8v4l6Xq8k6FeB4FBgtjE9PkNAujVBMDU5Nky7Qky0Qjy0RjE3PDNAPjVAPL63yn0xoCGRlWWlUxMjJAIIgvwWBmlmxubmRmaZKWaGKQbJFqkZKaYplsaszAAABWbyKk",
+    eventId: "66c772694fa40c8e8ded9c53",
+  },
+  {
+    tokenId:
+      "007eJxTYGBm9F7Q5DlXoal492zZv4r8jFyeTu6rTtRKTdzXUbEmwUuBwcLYxDQ5zcIo1cTA1CTZMi3JMtHIMtHYxNwwzcA41cDSlv90WkMgI0NAmw8LIwMEgvgSDGZmyeaJ5qZmJmmJJgbJFqkWKakpicbJxgwMAHbcIJs=",
+    eventId: "66c7a7564fa40c8e8deda3c3",
+  },
+  {
+    tokenId:'007eJxTYGjILxLnflYj8DgqhkNWf4t1xfXj77PWHnLfw7A1RP/UpwMKDBbGJqbJaRZGqSYGpibJlmlJlolGlonGJuaGaQbGqQaWf/hPpzUEMjII5e9gZWSAQBBfgsHMLNkizcjSJDkpxTQx2dg8zTzVMM0o1ZKBAQC6UyRL',
+    eventId: "66c8f294cbd5ac37f7e1f2e9"
+  },
+  {
+    tokenId:'007eJxTYOCqrNNae2vmEuZlkyZI3dqy2TJt2kapyuedG6JDHtt9nrxfgcHC2MQ0Oc3CKNXEwNQk2TItyTLRyDLR2MTcMM3AONXAMkXwdFpDICNDH08sIyMDBIL4EgxmZskWKckpFmbGaZZpaSkpqUCtJslpiQwMACwYJZA=',
+    eventId:'66c8dcd863f9ffddeb9a4cfa'
+  }
+];
+
 const JoinAudience = ({ eventId, liveStreamDetail }) => {
   const router = useRouter();
   const [joined, setJoined] = useState(false);
@@ -47,7 +68,7 @@ const JoinAudience = ({ eventId, liveStreamDetail }) => {
             // checkIfEventDurationHaveEnded &&
             newStatus === "Host has left the stream"
           ) {
-            router.push("/livestream/ended");
+            router.replace("/livestream/ended");
           }
         }
       );
@@ -64,8 +85,9 @@ const JoinAudience = ({ eventId, liveStreamDetail }) => {
   }, [eventId, checkIfEventDurationHaveEnded]);
 
   const handleJoin = async () => {
+   let token  = showTokens?.find((item) => item.eventId === eventId)?.tokenId;
     try {
-      await joinChannel(eventId);
+      await joinChannel(eventId, token);
       setJoined(true);
       console.log("Joined channel as audience");
     } catch (error) {
